2571e6b17bbc0271f63e055bfc935e615e10249b
kippsss <keithchewzk@gmail.com>
2024-08-18 18:00:19 +0800

    starting skills component

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Section from "../common/Section";
+import { SKILLS } from "./constants";
+import styles from "./Skills.module.css";
 
 function Skills() {
   return (
     <Section header="what i've learnt">
-      <div>hi</div>
+      <div className={styles.skillsContainer}>
+        {SKILLS.map((item) => {
+          return (
+            <div key={item.skill} className={styles.skillContainer}>
+              <span className={styles.skillText}>{item.skill}</span>
+            </div>
+          );
+        })}
+      </div>
     </Section>
   );
 }
