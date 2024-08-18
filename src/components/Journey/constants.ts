3ce904fc58ae937c6ce13c3f8d634e87806dde9d
kippsss <keithchewzk@gmail.com>
2024-08-18 16:12:38 +0800

    fill appropirate icons

--- src/components/Journey/constants.ts
+++ src/components/Journey/constants.ts
@@ -1,38 +1,34 @@
 export const JOURNEY_ITEMS = [
   {
     title: "AI Engineer at MSD",
-    description:
-      "Developed and maintained web applications, improving user experience and performance.",
-    startDate: "Jan 2018",
-    endDate: "Dec 2010",
+    description: "Third rotation: Hyperautomation & AI team",
+    startDate: "Jul 2024",
+    endDate: "Present",
     iconPath:
-      "https://heyitskeith-assets.s3.ap-southeast-1.amazonaws.com/cloud-filled.svg",
+      "https://heyitskeith-assets.s3.ap-southeast-1.amazonaws.com/robot-filled.svg",
   },
   {
     title: "Software Engineer at MSD",
-    description:
-      "Developed and maintained web applications, improving user experience and performance.",
-    startDate: "Jan 2018",
-    endDate: "Dec 2010",
+    description: "Second rotation: Global Software Development",
+    startDate: "Jan 2024",
+    endDate: "Jun 2024",
     iconPath:
-      "https://heyitskeith-assets.s3.ap-southeast-1.amazonaws.com/cloud-filled.svg",
+      "https://heyitskeith-assets.s3.ap-southeast-1.amazonaws.com/coding-filled.svg",
   },
   {
     title: "Cloud Engineer at MSD",
-    description:
-      "Developed and maintained web applications, improving user experience and performance.",
-    startDate: "Jan 2018",
-    endDate: "Dec 2010",
+    description: "First rotation: Cloud Business Office",
+    startDate: "July 2023",
+    endDate: "Dec 2024",
     iconPath:
       "https://heyitskeith-assets.s3.ap-southeast-1.amazonaws.com/cloud-filled.svg",
   },
   {
     title: "Graduated from NUS",
-    description:
-      "Developed and maintained web applications, improving user experience and performance.",
-    startDate: "Jan 2018",
-    endDate: "Dec 2010",
+    description: "Graduated with a bachelor degree in Computational Biology",
+    startDate: "May 2023",
+    endDate: "May 2023",
     iconPath:
-      "https://heyitskeith-assets.s3.ap-southeast-1.amazonaws.com/cloud-filled.svg",
+      "https://heyitskeith-assets.s3.ap-southeast-1.amazonaws.com/graduation-cap-filled.svg",
   },
 ];
