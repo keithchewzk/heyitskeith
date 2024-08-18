1c1c901cd9e8d535adc617d0c4d3f0673aeb3b3f
kippsss <keithchewzk@gmail.com>
2024-08-18 17:20:28 +0800

    add starting skills as constants

---
+++ src/components/Skills/constants.ts
@@ -0,0 +1,15 @@
+export const SKILLS = [
+  { skill: "Python", categories: ["languages"] },
+  { skill: "HTML", categories: ["languages"] },
+  { skill: "CSS", categories: ["languages"] },
+  { skill: "JavaScript", categories: ["languages"] },
+  { skill: "NLP", categories: ["concepts"] },
+  { skill: "AI", categories: ["concepts"] },
+  { skill: "Data Science", categories: ["concepts"] },
+  { skill: "Bioinformatics", categories: ["concepts"] },
+  { skill: "AWS", categories: ["tools"] },
+  { skill: "Git", categories: ["tools"] },
+  { skill: "ReactJS", categories: ["frameworks"] },
+  { skill: "NextJS", categories: ["frameworks"] },
+  { skill: "ReactNative", categories: ["frameworks"] },
+];
