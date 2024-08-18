e2b915b5b403163e1ea2e9442b834a749331afa7
kippsss <keithchewzk@gmail.com>
2024-08-18 14:41:10 +0800

    add borderless cloud icon

--- src/components/Journey/ExperienceIcon.tsx
+++ src/components/Journey/ExperienceIcon.tsx
@@ -9,8 +9,9 @@ function ExperienceIcon({ path }: Props) {
   return (
     <div className={styles.experienceIconContainer}>
       <img
-        src="https://heyitskeith-icons.s3.amazonaws.com/cloud.svg"
-        alt="some alt"
+        src="https://heyitskeith-assets.s3.ap-southeast-1.amazonaws.com/cloud.svg"
+        alt=""
+        className={styles.experienceIcon}
       />
     </div>
   );
