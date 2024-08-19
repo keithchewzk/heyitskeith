9e66316dfb7830f59fd3b89fa9783374c0d63bbf
kippsss <keithchewzk@gmail.com>
2024-08-19 19:05:03 +0800

    add span to Go To text

--- src/components/AppBar/GoTos.tsx
+++ src/components/AppBar/GoTos.tsx
@@ -4,10 +4,18 @@ import styles from "./GoTos.module.css";
 function GoTos() {
   return (
     <div className={styles.goTosContainer}>
-      <div className={styles.goToText}>About</div>
-      <div className={styles.goToText}>Experiences</div>
-      <div className={styles.goToText}>Skills</div>
-      <div className={styles.goToText}>AIPlayground</div>
+      <div className={styles.goToText}>
+        <span>About</span>
+      </div>
+      <div className={styles.goToText}>
+        <span>Experiences</span>
+      </div>
+      <div className={styles.goToText}>
+        <span>Skills</span>
+      </div>
+      <div className={styles.goToText}>
+        <span>AIPlayground</span>
+      </div>
     </div>
   );
 }
