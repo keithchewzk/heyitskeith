064f12282c82f5590d17d54794b51cada606bbb5
kippsss <keithchewzk@gmail.com>
2024-08-19 18:22:37 +0800

    hover over skill category selects proper skill

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,21 +1,40 @@
 import React from "react";
 import Section from "../common/Section";
+import { useState } from "react";
 import { CATEGORIES, SKILLS } from "./constants";
 import styles from "./Skills.module.css";
 
 function Skills() {
+  const [category, setCategory] = useState("all");
+  console.log(category);
   return (
     <Section header="Skills">
       <div className={styles.categoriesContainer}>
         {CATEGORIES.map((item) => {
-          return <button className={styles.categoryButton}>{item}</button>;
+          return (
+            <button
+              onMouseEnter={() => setCategory(item)}
+              onMouseLeave={() => setCategory("all")}
+              className={styles.categoryButton}
+            >
+              {item}
+            </button>
+          );
         })}
       </div>
       <div className={styles.skillsContainer}>
         {SKILLS.map((item) => {
           return (
             <div key={item.skill} className={styles.skillContainer}>
-              <span className={styles.skillText}>{item.skill}</span>
+              <span
+                style={{
+                  opacity:
+                    item.category == category || category == "all" ? 1 : 0.05,
+                }}
+                className={styles.skillText}
+              >
+                {item.skill}
+              </span>
             </div>
           );
         })}
