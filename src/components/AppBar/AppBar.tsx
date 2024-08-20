94128510827092606cf3e985057b468fe7df50c3
kippsss <keithchewzk@gmail.com>
2024-08-20 23:17:10 +0800

    set basic non functional switch

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./AppBar.module.css";
 import GoTos from "./GoTos";
+import Switch from "../common/Switch";
 
 function AppBar() {
+  const [switchOn, setSwitchOn] = useState();
   return (
     <div className={styles.appBarContainer}>
       <p className={styles.brandText}>heyItsKeith</p>
       <GoTos />
+      <Switch value={false} onClick={() => {}} />
     </div>
   );
 }
