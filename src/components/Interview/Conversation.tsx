bea6cedb1e9cf9f5c1527f2fea6f0a54a86527e5
kippsss <keithchewzk@gmail.com>
2024-08-24 10:09:30 +0800

    add border and backgound to message container

--- src/components/Interview/Conversation.tsx
+++ src/components/Interview/Conversation.tsx
@@ -14,8 +14,21 @@ interface Props {
 function Conversation({ conversation, addToConversation }: Props) {
   return (
     <div className={styles.conversationContainer}>
-      {conversation.map((messageItem: Message) => {
-        return <div>conversation item</div>;
+      {conversation.map((messageItem: Message, index) => {
+        const role = Object.keys(messageItem)[0];
+        const message = Object.values(messageItem)[0];
+        return (
+          <div
+            key={index}
+            className={
+              role === "user" ? styles.messageRowLeft : styles.messageRowRight
+            }
+          >
+            <div className={styles.messageContainer}>
+              <span className={styles.message}>{message}</span>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
