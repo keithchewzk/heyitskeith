f5b9e0fc4773223d4a849cf6c929af5157857cea
kippsss <keithchewzk@gmail.com>
2024-08-28 21:12:22 +0800

    change hero wording

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,13 +1,12 @@
 import React from "react";
+import { greeting, header, subheader } from "./constants";
 
 function Hero() {
   return (
     <div>
-      <p className="text-6xl font-medium mb-2">Keith Chew</p>
-      <p className="text-2xl mb-4">Aspiring AI and Software Engineer</p>
-      <p className="text-lg">
-        I like to code, learn new technologies, and build software.
-      </p>
+      <p className="text-8xl font-medium mb-4">{greeting}</p>
+      <p className="text-6xl font-medium mb-4">{header}</p>
+      <p className="text-lg">{subheader}</p>
     </div>
   );
 }
---
+++ src/components/Hero/constants.ts
@@ -0,0 +1,6 @@
+export const greeting = "Hello!";
+
+export const header = "My name is Keith";
+
+export const subheader =
+  "I like to code, learn new technologies, and build meaningful software.";
