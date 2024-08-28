a00ccf0670281297fc67dca4ea3b1d99c8791828
kippsss <keithchewzk@gmail.com>
2024-08-28 23:05:28 +0800

    add id to skill

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,7 @@ import { aboutMeParaOne, aboutMeParaTwo } from "./constants";
 
 function About() {
   return (
-    <div className="mb-20">
+    <div className="mb-28">
       <p className="mb-5 text-text-secondary">{aboutMeParaOne}</p>
       <p className="text-text-secondary">{aboutMeParaTwo}</p>
     </div>
--- src/components/Experience/Skills.tsx
+++ src/components/Experience/Skills.tsx
@@ -7,7 +7,10 @@ function Skills({ skills }: Props) {
     <div>
       {skills.map((skill) => {
         return (
-          <span className="inline-block text-md rounded-lg font-medium border border-solid border-border-primary bg-button-secondary text-text-secondary leading-6 px-3 pt-1 mb-3 mr-3">
+          <span
+            key={skill}
+            className="inline-block text-md rounded-lg font-medium border border-solid border-border-primary bg-button-secondary text-text-secondary leading-6 px-3 pt-1 mb-3 mr-3"
+          >
             {skill}
           </span>
         );
