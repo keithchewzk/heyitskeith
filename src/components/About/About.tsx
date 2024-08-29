377c367765cbe14f48883bbf560de63884a1ac7a
kippsss <keithchewzk@gmail.com>
2024-08-29 23:42:21 +0800

    simple about animation

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { aboutMeParaOne, aboutMeParaTwo } from "./constants";
+import { motion } from "framer-motion";
 
 function About() {
   return (
-    <div className="mb-28">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      className="mb-28"
+    >
       <p className="mb-5 text-text-secondary">{aboutMeParaOne}</p>
       <p className="text-text-secondary">{aboutMeParaTwo}</p>
-    </div>
+    </motion.div>
   );
 }
 
