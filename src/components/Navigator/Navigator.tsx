ea0578012f67172b905c4ad73f17d2e600b5a982
kippsss <keithchewzk@gmail.com>
2024-09-02 22:49:08 +0800

    selectable section

--- src/components/Navigator/Navigator.tsx
+++ src/components/Navigator/Navigator.tsx
@@ -1,16 +1,37 @@
-import React, { useRef, useState } from "react";
-import { motion, useMotionValueEvent, useScroll } from "framer-motion";
-import Section from "./Section";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 function Navigator() {
   const sections = ["About", "Experience", "Interview me"];
   const [selectedSection, setSelectedSection] = useState("About");
+  const relativeDist =
+    (sections.indexOf(selectedSection) * 100) / sections.length;
 
   return (
-    <div className="flex flex-col">
-      {sections.map((section) => {
-        return <Section section={section} />;
-      })}
+    <div className="flex flex-row">
+      <div className="w-1 mr-4 bg-border-primary rounded-2xl">
+        <motion.div
+          animate={{ top: `${relativeDist}%` }}
+          className={`w-1 bg-border-selected relative rounded-2xl h-1/${sections.length}`}
+        >
+          &nbsp;
+        </motion.div>
+      </div>
+      <div>
+        {sections.map((section) => {
+          return (
+            <motion.div
+              className="text-text-primary cursor-pointer text-lg font-medium p-0.5"
+              whileHover={{ x: 7 }}
+              onClick={() => {
+                setSelectedSection(section);
+              }}
+            >
+              {section}
+            </motion.div>
+          );
+        })}
+      </div>
     </div>
   );
 }
