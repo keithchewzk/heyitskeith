7971092b639ea07a3cc309598a550f14ee7865fa
kippsss <keithchewzk@gmail.com>
2024-09-06 21:28:54 +0800

    add exeprience constants

---
+++ src/components/Experience/constants.ts
@@ -0,0 +1,45 @@
+export const jobs = [
+  {
+    id: 0,
+    role: "Undergraduate",
+    company: "NUS",
+    description: "Graduated with a bachelor's degree in Computational Biology",
+    dateRange: "Aug 2019 - May 2023",
+    skills: [
+      "Bioinformatics",
+      "NLP",
+      "Data Science",
+      "Databases",
+      "AI/ML",
+      "Genomics",
+      "Big Data",
+    ],
+  },
+  {
+    id: 1,
+    role: "Cloud Engineer",
+    company: "MSD",
+    description:
+      "My first rotation was with the Cloud Business Office as a Cloud Engineer. I developed cloud automation tools that significantly enhanced decision-making within the team. I engineered solutions for cloud products that improved visibility into cloud costs, enabling more informed and strategic actions to optimize resource usage. My contributions helped streamline cloud operations and reduce unnecessary expenditures, driving overall efficiency and cost-effectiveness in managing the company's cloud infrastructure.",
+    dateRange: "Jul 2023 - Dec 2024",
+    skills: ["Python", "AWS", "SQL"],
+  },
+  {
+    id: 2,
+    role: "Software Engineer",
+    company: "MSD",
+    description:
+      "My second rotation was in the Global Software Development team. I contributed to both full-stack web and mobile application development. I developed and maintained the company’s in-house link shortening and QR code generation software, handling both frontend and backend tasks to ensure a seamless user experience. Additionally, I played a key role in creating a mobile application for enterprise leadership skills, enhancing its functionality and usability. My responsibilities also included product management tasks, where I gathered user requirements, coordinated with stakeholders, and oversaw product delivery to ensure alignment with business objectives and user needs.",
+    dateRange: "Jan 2024 - Jun 2024",
+    skills: ["NextJS", "TypeScript", "React Native", "AWS"],
+  },
+  {
+    id: 3,
+    role: "AI Engineer",
+    company: "MSD",
+    description:
+      "During my third rotation, I joined the Hyperautomation & AI team, where I worked on engineering solutions for our company's enterprise generative AI software, talkGPTeal. My primary focus was on developing a robust backend repository and enhancing the current model through prompt engineering and Retrieval-Augmented Generation (RAG) techniques. I successfully improved talkGPTeal's capability to search through internal company data by implementing RAG methods, contributing to more efficient data retrieval and model performance.",
+    dateRange: "Jul 2024 - Present",
+    skills: ["Python", "Backend", "Prompt Eng.", "RAG", "APIs"],
+  },
+];
