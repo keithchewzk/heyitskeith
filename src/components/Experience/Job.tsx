dc5c36c23b954ebe8e5f39a281fd9690b0143d47
kippsss <keithchewzk@gmail.com>
2024-09-14 09:32:22 +0800

    remove animation from jobs

--- src/components/Experience/Job.tsx
+++ src/components/Experience/Job.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Skills from "./Skills";
 
 interface Props {
@@ -18,33 +17,24 @@ function Job({
   skills,
   company,
 }: Props) {
-  const [showAdditionalDescription, setShowAdditionalDescription] =
-    useState<boolean>(false);
-
   return (
-    <div className="mb-10 opacity-0 animate-fade-in translate-x-5">
+    <div className="mb-10 opacity-0 animate-fade-in">
       <p className="text-lg text-text-tertiary font-semibold mb-1">
         {dateRange}
       </p>
       <p className="text-2xl font-medium">{role}</p>
       <p className="text-xl mb-3">{company}</p>
-      <div
-        className="mb-3 -m-2 p-2 cursor-pointer"
-        onMouseEnter={() => setShowAdditionalDescription(true)}
-        onMouseLeave={() => setShowAdditionalDescription(false)}
-      >
+      <div className="mb-3 -m-2 p-2">
         <p className="text-md text-text-secondary mb-2">{description}</p>
-        {showAdditionalDescription && (
-          <div className="transition-all duration-300 ease-out overflow-hidden">
-            {additionalDescription &&
-              additionalDescription.map((desc, index) => (
-                <div key={index} className="flex">
-                  <p className="mr-1">•</p>
-                  <p className="text-md text-text-secondary mb-1">{desc}</p>
-                </div>
-              ))}
-          </div>
-        )}
+        <div>
+          {additionalDescription &&
+            additionalDescription.map((desc, index) => (
+              <div key={index} className="flex">
+                <p className="mr-1">•</p>
+                <p className="text-md text-text-secondary mb-1">{desc}</p>
+              </div>
+            ))}
+        </div>
       </div>
       <Skills skills={skills} />
     </div>
