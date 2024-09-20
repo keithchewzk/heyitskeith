e84b66f400e622a87c835e00ed55341a9a607b2a
kippsss <keithchewzk@gmail.com>
2024-09-20 22:35:21 +0800

    move date range to side

--- src/components/Experience/Job.tsx
+++ src/components/Experience/Job.tsx
@@ -18,23 +18,27 @@ function Job({
   company,
 }: Props) {
   return (
-    <div className="mb-10 opacity-0 animate-fade-in">
-      <p className="text-lg text-text-muted font-semibold mb-1">{dateRange}</p>
-      <p className="text-2xl font-medium">{role}</p>
-      <p className="text-xl mb-3">{company}</p>
-      <div className="mb-3 -m-2 p-2">
-        <p className="text-md text-text-muted mb-2">{description}</p>
-        <div>
-          {additionalDescription &&
-            additionalDescription.map((desc, index) => (
-              <div key={index} className="flex">
-                <p className="mr-1">•</p>
-                <p className="text-md text-text-muted mb-1">{desc}</p>
-              </div>
-            ))}
-        </div>
+    <div className="flex flex-row mb-10 opacity-0 animate-fade-in">
+      <div className="mr-5 mt-1">
+        <p className="text-md text-text-muted text-nowrap">{dateRange}</p>
       </div>
-      <Skills skills={skills} />
+      <div>
+        <p className="text-2xl font-medium">{role}</p>
+        <p className="text-xl mb-3">{company}</p>
+        <div className="mb-3 -m-2 p-2">
+          <p className="text-md text-text-muted mb-2">{description}</p>
+          <div>
+            {additionalDescription &&
+              additionalDescription.map((desc, index) => (
+                <div key={index} className="flex">
+                  <p className="mr-1">•</p>
+                  <p className="text-md text-text-muted mb-1">{desc}</p>
+                </div>
+              ))}
+          </div>
+        </div>
+        <Skills skills={skills} />
+      </div>
     </div>
   );
 }
