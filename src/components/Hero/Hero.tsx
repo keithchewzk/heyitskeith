219e67b7b33ba0aa9cc1fc3cc5f9ea1bdc68eff8
kippsss <keithchewzk@gmail.com>
2024-09-21 11:59:04 +0800

    adjust hero text

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,8 @@
 function Hero() {
   return (
     <div className="mb-20">
-      <p className="text-7xl font-medium">
-        Hello
-        <br />
-        I'm
-      </p>
-      <p className="text-8xl font-medium mb-4">Keith</p>
+      <p className="text-9xl font-medium mb-6">Hello.</p>
+      <p className="text-8xl font-medium mb-6">I'm Keith</p>
       <p className="text-2xl text-text-muted">AI & Software Engineer</p>
     </div>
   );
