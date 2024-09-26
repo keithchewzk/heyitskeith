b3488b4c97e3df4edbe8d8bb4ee8a620388fa9d5
kippsss <keithchewzk@gmail.com>
2024-09-26 23:37:09 +0800

    change about me content

--- src/components/About/constants.ts
+++ src/components/About/constants.ts
@@ -1,4 +1,4 @@
 export const aboutMeParaOne =
-  "I discovered my passion for programming in university, where a module sparked my interest. Since then, I’ve pursued a variety of areas through MSD's Emerging Talent Rotation Programme, including full-stack development, AI engineering, and cloud architecture. ";
+  "Starting my university education with Life Sciences, I chanced upon my passion for programming when a computing module sparked my interest. This led me to switch my degree to Computational Biology, blending my interest in both life sciences and technology. Since then, I’ve pursued a variety of areas through MSD's Emerging Talent Rotation Programme, including full-stack development, AI engineering, and cloud architecture.";
 export const aboutMeParaTwo =
-  "As I’m early in my career, my focus is on building strong technical foundations, particularly in the fields of AI and software engineering. I’m always open to new opportunities that challenge me to learn and grow across the diverse landscape of technology.";
+  "As I’m early in my career, my focus is on building strong technical foundations, particularly in the fields of AI and software engineering. I’m always open to new opportunities that challenge me to learn and grow across the diverse landscape IT.";
