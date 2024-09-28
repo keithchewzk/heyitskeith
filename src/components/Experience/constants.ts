61cb133d8510614839ec84fb05eb5dcec6af814c
kippsss <keithchewzk@gmail.com>
2024-09-28 09:52:29 +0800

    change content of about and fixed typo in undergrad experience

--- src/components/Experience/constants.ts
+++ src/components/Experience/constants.ts
@@ -4,7 +4,7 @@ export const JOBS = [
     role: "Undergraduate",
     company: "NUS",
     description:
-      "I pursued a Bachelor's degree in Computational Biology, studying a mix of biology and computer science modules. Along the wau, I realised I enjoyed building software more than doing scientific research, which led me to where I am today in the tech industry.",
+      "I pursued a Bachelor's degree in Computational Biology, studying a mix of biology and computer science modules. Along the way, I found that I enjoyed building software more than doing scientic research, which led me to where I am today in the tech industry.",
     additionalDescription: undefined,
     dateRange: "Aug 2019 - May 2023",
     skills: [
